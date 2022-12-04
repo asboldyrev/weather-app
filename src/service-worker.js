c67656ec7d83dffbefbe18579ec9544f061965c0
asboldyrev <angelcorpc@ya.ru>
2022-12-04 22:05:25 +0700

    Добавил сервис воркер

---
+++ src/service-worker.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-console */
+
+if (process.env.NODE_ENV === 'production') {
+	const staticCacheName = 'v1.0.0-s'
+	const dynamicCacheName = 'v1.0.0-d'
+
+	const assetUrls = [
+		'/',
+		'/js/app.js',
+		'/img/icon-512x512.png',
+		'/img/icon-384x384.png',
+		'/img/icon-256x256.png',
+		'/img/icon-192x192.png',
+		'/img/icons/colored-fill/not-available.svg',
+		'/img/icons/colored-line/barometer.svg',
+		'/img/icons/colored-line/windsock.svg',
+		'/img/icons/colored-line/humidity.svg',
+		'/img/icons/colored-line/compass.svg',
+	];
+
+	self.addEventListener('install', async event => {
+		const cache = await caches.open(staticCacheName)
+		await cache.addAll(assetUrls)
+	})
+
+	self.addEventListener('activate', async event => {
+		const cacheNames = await caches.keys()
+		await Promise.all(
+			cacheNames
+				.filter(name => name !== staticCacheName)
+				.filter(name => name !== dynamicCacheName)
+				.map(name => caches.delete(name))
+		)
+	})
+
+	self.addEventListener('fetch', async event => {
+		const { request } = event
+
+		const url = new URL(request.url)
+
+		if (
+			assetUrls.indexOf(url.pathname) != -1 &&
+			url.pathname.indexOf('api') == -1 &&
+			url.pathname.indexOf('browser-sync') == -1
+		) {
+			event.respondWith(cacheFirst(request))
+		} else if (
+			url.pathname.indexOf('api') == -1 &&
+			url.pathname.indexOf('browser-sync') == -1
+		) {
+			event.respondWith(networkFirst(request))
+		} else if (
+			url.pathname.indexOf('api') != -1
+		) {
+			try {
+				const response = await fetch(request)
+				return response ?? await caches.match('/offline')
+			} catch (e) {
+				//
+			}
+		}
+	})
+
+	async function cacheFirst(request) {
+		const cached = await caches.match(request)
+		return cached ?? await fetch(request)
+	}
+
+	async function networkFirst(request) {
+		const cache = await caches.open(dynamicCacheName)
+		try {
+			const response = await fetch(request)
+			await cache.put(request, response.clone())
+			return response
+		} catch (e) {
+			const cached = await cache.match(request)
+			return cached ?? await caches.match('/offline')
+		}
+	}
+}
