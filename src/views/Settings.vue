9ec64e49aeb792ef444d5ad11448b4fcc0cee9ae
asboldyrev <angelcorpc@ya.ru>
2022-12-07 20:52:22 +0700

    Доверстал представление настроек

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -17,43 +17,97 @@ import About from '../components/About.vue'
 	<div class="search">
 		<Search />
 	</div>
-	<div class="current-city">
-		<span class="current-city__label">Current City</span>: <span>{{ cityStore.getCity()?.name }} ({{ cityStore.getCity()?.country }})</span>
+
+	<div class="settings">
+		<div class="settings__item">
+			<div class="settings__item_name">Current City</div>
+			<div class="settings__item_value">
+				{{ cityStore.getCity()?.name }} ({{ cityStore.getCity()?.country }})
+			</div>
+		</div>
+		<div class="settings__item">
+			<div class="settings__item_name">Timezone</div>
+			<div class="settings__item_value">
+				<div class="timezone">
+					<span class="timezone__label_before">City</span>
+					<input
+						type="checkbox"
+						class="timezone__input"
+						@click="setTimezone"
+						:checked="settingsStore.getSetting('localTimezone')"
+					>
+					<span class="timezone__label_after">Local</span>
+				</div>
+			</div>
+		</div>
 	</div>
 
-	<label class="timezone">
-		<input
-			type="checkbox"
-			id="timezone-input"
-			class="timezone__input"
-			@click="setTimezone"
-			:checked="settingsStore.getSetting('localTimezone')"
-		>
-		<label for="timezone-input">Local timezone</label>
-	</label>
 	<div class="about">
 		<About />
 	</div>
 </template>
 
 <style lang="scss" scoped>
+	.search {
+		margin-bottom: 24rem;
+	}
+
+	.settings {
+		&__item {
+			box-sizing: border-box;
+			display: grid;
+			grid-template-columns: 1fr auto;
+			grid-auto-columns: 1fr;
+			grid-auto-rows: 1fr;
+			gap: 0px 0px;
+			grid-auto-flow: row;
+			justify-items: stretch;
+			border-top: 1rem solid var(--comment);
+			padding: 10rem 0;
+
+			&:first-child {
+				border: none;
+			}
+
+			&_name {
+				color: var(--cyan);
+				font-weight: 600;
+			}
+		}
+	}
+
+
+	.timezone {
+		display: grid;
+		grid-template-columns: repeat(3, auto);
+
+		&__input {
+			-webkit-appearance: none;
+			-moz-appearance: none;
+			-o-appearance: none;
+			width: 40rem;
+			height: 20rem;
+			background-color:var(--foreground);
+			border: 1rem solid var(--cyan);
+			border-radius: 50rem;
+			-webkit-box-shadow: inset -20rem 0rem 0rem 0rem var(--cyan);
+			box-shadow: inset -20rem 0rem 0rem 0rem var(--cyan);
+			-webkit-transition-duration: 200ms;
+			transition-duration: 200ms;
+			padding: 0;
+			margin: 0 10rem;
+
+			&:checked {
+				-webkit-box-shadow: inset 20rem 0rem 0rem 1rem var(--orange);
+				box-shadow: inset 20rem 0rem 0rem 1rem var(--orange);
+				border: 1rem solid var(--orange);
+			}
+		}
+	}
+
 	.about {
 		margin-top: 35rem;
 		text-align: center;
 		display: block;
-	}
-
-	.current-city {
-		margin: 10rem 15rem;
-		margin-left: 0;
-
-		&__label {
-			color: var(--cyan);
-			font-weight: 600;
-		}
-	}
-
-	.timezone {
-
 	}
 </style>
