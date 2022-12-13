6ab84f7aa2baece1030a1e6c5a2c3e25da14ef31
asboldyrev <angelcorpc@ya.ru>
2022-12-13 23:48:32 +0700

    Добавил хранилище качества воздуха

---
+++ src/stores/airQuality.js
@@ -0,0 +1,57 @@
+import { defineStore } from "pinia";
+import { computed, reactive, ref } from "vue";
+import { airQuality } from "../use/openMeteoProperties";
+
+import { useCityStore } from './city'
+import { useSettingsStore } from './settings'
+
+export const useAirQualityStore = defineStore('airQuality', () => {
+	const cityStore = useCityStore();
+	const settingsStore = useSettingsStore();
+
+	const params = reactive({
+		latitude: cityStore.city.latitude,
+		longitude: cityStore.city.longitude,
+		timezone: settingsStore.timezone,
+		hourly: airQuality(),
+	});
+
+	let _quality = ref({});
+
+	async function update() {
+		if (cityStore.getCityField('latitude')) {
+			params.latitude = cityStore.getCityField('latitude');
+			params.longitude = cityStore.getCityField('longitude');
+			params.timezone = settingsStore.timezone;
+
+			await fetch(decodeURIComponent(`https://air-quality-api.open-meteo.com/v1/air-quality?${new URLSearchParams(params).toString()}`))
+				.then(response => response.json())
+				.then(response => {
+					_quality.value = response;
+				});
+		}
+	}
+
+	function getHourlyValue(name) {
+		if (_quality.value?.hourly) {
+			return _quality.value?.hourly[name][settingsStore.currentHour];
+		}
+	}
+
+	function getHourlyUnit(name) {
+		if (_quality.value?.hourly_units) {
+			return _quality.value?.hourly_units[name];
+		}
+	}
+
+	const quality = computed(() => {
+		return _quality.value;
+	});
+
+	return {
+		update,
+		getHourlyValue,
+		getHourlyUnit,
+		quality,
+	}
+})
